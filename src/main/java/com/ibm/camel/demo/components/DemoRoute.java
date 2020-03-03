1909f2f7e938dec13dc7ef202aab4b73fbf361cb
Lung Shyh Shen <lungss@my.ibm.com>
2020-03-03 11:39:37 +0800

    updated url

--- src/main/java/com/ibm/camel/demo/components/DemoRoute.java
+++ src/main/java/com/ibm/camel/demo/components/DemoRoute.java
@@ -17,10 +17,10 @@ public class DemoRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        restConfiguration().component("netty-http").host("0.0.0.0").port(8080).bindingMode(RestBindingMode.json);
+        restConfiguration().component("netty-http").host("0.0.0.0").port(80).bindingMode(RestBindingMode.json);
 //        .setJsonDataFormat("json-jackson");
 
-        from("rest://post:echo").unmarshal().json(JsonLibrary.Jackson).process(new Processor() {
+        from("rest://post:2echo").unmarshal().json(JsonLibrary.Jackson).process(new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
                 System.out.println(exchange.getIn().getBody());
@@ -32,7 +32,7 @@ public class DemoRoute extends RouteBuilder {
         // .to("http://localhost:8080/echoGet/helo?bridgeEndpoint=true");
         .to("http://istio-ingressgateway-istio-system.mycluster-383951-d4a42c1d53effecb3a546f2cced5589b-0000.sng01.containers.appdomain.cloud/echoget/helloworld?bridgeEndpoint=true");
 
-        from("rest://get:echoGet/{echoValue}").process(new Processor() {
+        from("rest://get:2echoGet/{echoValue}").process(new Processor() {
             @Override
             public void process(Exchange exchange) throws Exception {
                 System.out.println(exchange.getIn().getHeader("echoValue"));
